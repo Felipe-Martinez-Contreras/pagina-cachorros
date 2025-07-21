7f72b7f91c5eb3a22b3d9601213a1bb267c03b08
FelipeM <fmartinez19@alumnos.utalca.cl>
2025-07-21 02:08:35 -0400

    Pagina funcional, falta corregir carrusel, no funciona

---
+++ src/app/inicio/inicio.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './inicio.html',
+  styleUrl: './inicio.css'
+})
+export class Inicio {
+
+}
